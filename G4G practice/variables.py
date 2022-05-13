9522e6967a0adec11a8a1a75ce685eb61f562d6e
retrouvm <retrouvaillesm@gmail.com>
2022-05-13 15:30:50 -0500

    test

---
+++ G4G practice/variables.py
@@ -0,0 +1,30 @@
+#introduction of concepts of variables in Python
+
+
+age=38
+name="ankit"
+weight=58.5
+print(age)
+print(name)
+print(weight, "\n")
+
+
+x=10
+y="geek"
+z=20
+w=z
+w=30
+print(x, y, z, w, "\n")
+
+is_valid=True
+marks=90
+pi=3.14
+city_name="noida"
+print(is_valid)
+print(marks)
+print(pi)
+print(city_name, "\n")
+
+x=None
+print(x, "\n")
+
