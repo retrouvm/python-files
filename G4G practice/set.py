c8335703b405c69e34dda8b810d32fb657e8aba9
retrouvm <retrouvaillesm@gmail.com>
2022-05-16 05:17:03 -0500

    length

--- G4G practice/set.py
+++ G4G practice/set.py
@@ -13,7 +13,46 @@ s3={} #empty braces is a dictionary
 print(type(s3))
 s4=set() #for empty set you have to use set()
 print(type(s4))
-print(s4)
+print(s4,"\n")
+################################################################################################################
+
+################################################################################################################
+#add, update
+
+s={10,20}
+s.add(30) #adds value into end of set
+print(s)
+s.add(30) #cannot add duplicate values
+print(s) #set does not change, since no two items can be the same
+
+s.update ([40,50]) #using the update function we can add items from other collections such as lists/tuples
+print(s) #the order in which the items are inserted into the set does not follow a certain pattern, it is unpredictables
+
+s.update({60,70},[80,90]) #you can insert items from different types of collections at the same time
+print(s,"\n")
+################################################################################################################
+
+################################################################################################################
+#discard, remove, clear, delete
+
+s={10,30,20,40}
+s.discard(30) #if value exists within set, it is removed, otherwise nothing happens and set remains as is
+print(s)
+s.remove(20) #similar to discard but with remove if value does not exist within set, then error is raised
+print(s)
+s.clear() #everything is removed and you're left with an empty set, set(), not {}
+print(s,"\n")
+s.add(50) #adds value (50) to previously empty set 
+del s #completely removes object, meaning it cannot be referenced anymore, you cannot add to it 
+################################################################################################################
+
+################################################################################################################
+#length, existence
+
+s={10,30,20,40}
+print(len(s)) #returns size of set, num of elements
+print(20 in s) #returns T/F if value exists within set
+print(50 in s, "\n") #returns T/F if value exists within set
 ################################################################################################################
 
 ################################################################################################################
